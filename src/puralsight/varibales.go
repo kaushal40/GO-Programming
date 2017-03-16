aa11cb6e6fa4c15078603c47276752de53aacb12
kaushal40 <kaushalprajapati46@gmail.com>
2017-03-15 20:48:47 -0500

    varibales and types

---
+++ src/puralsight/varibales.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+)
+
+// declare variabale outside must use var keyword
+
+var (
+	name string = "kaushal"
+	//automatic type assignment
+	number  = 10.7
+	number1 = 10.0
+	number2 = 11
+)
+
+func main() {
+	fmt.Println("hello frm", runtime.GOOS)
+	fmt.Println("name is", name, " varible type is", reflect.TypeOf(name))
+	fmt.Println("number is", number, " varible type is", reflect.TypeOf(number))
+	//number1 + number2 will give an error
+	fmt.Println("sum is", int(number1)+number2)
+}
